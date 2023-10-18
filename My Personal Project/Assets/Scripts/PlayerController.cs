524c4ffe7c772c6db425db31f9d11bb5efb2e7c6
DenysRudenko <heartbreathing19951014@gmail.com>
2023-10-18 21:49:17 +0100

    Created controls for player.

--- My Personal Project/Assets/Scripts/PlayerController.cs
+++ My Personal Project/Assets/Scripts/PlayerController.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
+{   
+    private float speed = 10.0f;
+
+    private Rigidbody playerRb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();   
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   // controll for arrow keys
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
     }
 }
